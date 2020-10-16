c428ac1994d7ff804879ecc4fde0124445d9952b
Lucas Montanari <lmonty2011@gmail.com>
2020-10-16 18:23:39 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
 # Write your solution here
+# Put time in Epoch time (total seconds since jan 1st 1970)
+time = Time.now.to_i
+
+# if statement to see if the seconds are in odd or even
+if(time % 2 == 0)
+  puts "Even!"
+else 
+  puts "Odd!"
+end
